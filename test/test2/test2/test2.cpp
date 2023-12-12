ce321e535c94e90e6a48c893011e2b8130ec86d4
Teddeakin <147064059+Teddeakin@users.noreply.github.com>
2023-12-12 13:38:42 +0000

    s

--- test/test2/test2/test2.cpp
+++ test/test2/test2/test2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 void menu();
@@ -14,28 +13,36 @@ int main() {
 	int uanswer = 0;
 	int answer = 0;
 
-	while (uanswer != 5) {
-		menu();
+	while (uanswer != 5) { // while loop to keep repeating unless the user enters 5 when propmted in menu
+		menu(); // calls on the menu function
 
-		while (!(cin >> uanswer) || uanswer > 5 || uanswer <= 0) {
+		while (!(cin >> uanswer) || uanswer > 5 || uanswer <= 0) { // a while loop that repeats if the input isn't a number or is above 5 or below 1
 			cout << "please enter valid number" << endl;
-			menu();
-			cin.clear();
+			menu(); // calls on the menu function
+			cin.clear(); // clears the previous input so that it can be repeated
 			cin.ignore(123, '\n');
 		}
 
 		if (uanswer == 5) {
-			return 0;
+			return 0; // if the user inputs 5 after menu displays it will exit the program
 		}
 
-		cout << "are you using 2 or 3 numbers?" << endl;
-		while (!(cin >> answer) || answer > 3 || answer <= 1) {
-			cout << "please enter valid number" << endl;
-			cout << "are you using 2 or 3 numbers?" << endl;
-			cin.clear();
-			cin.ignore(123, '\n');
-		}
-		if (answer == 2) {
+		
+			try {
+				cout << "are you using 2 or 3 numbers?" << endl;
+				cin >> answer;
+				if (cin.fail() || answer > 3 || answer <= 1) {
+					throw 505;
+				}
+			}
+			catch (...) {
+				cout << "please enter a valid number" << endl;
+				cin.clear();
+				cin.ignore(123, '\n');
+			}
+		
+
+		if (answer == 2) { // if the user wants to use 2 numbers
 			cout << "input two numbers to use in calculation" << endl;
 			while (!(cin >> uinput1) || !(cin >> uinput2)) {
 				cout << "please enter valid numbers" << endl;
@@ -43,12 +50,12 @@ int main() {
 				cin.ignore(123, '\n');
 			}
 
-			calculation(uinput1, uinput2, uanswer);
+			calculation(uinput1, uinput2, uanswer); // calls on the calculation function to use the numbers inputted by user for calculation
 
-			double returnedTotal = calculation(uinput1, uinput2, uanswer);
-			cout << returnedTotal << endl << endl;
+			double returnedTotal = calculation(uinput1, uinput2, uanswer); // makes this value equal to the returned value (total) from the function
+			cout << returnedTotal << endl << endl;// two endl to separate this from the next calculation
 		}
-		if (answer == 3) {
+		if (answer == 3) { // if the user wants to use three numbers
 			cout << "input three numbers to use in calculation" << endl;
 			while (!(cin >> uinput1) || !(cin >> uinput2) || !(cin >> uinput3)) {
 				cout << "please enter valid numbers" << endl;
@@ -56,7 +63,7 @@ int main() {
 				cin.ignore(123, '\n');
 			}
 			cout << "what would you like to use for third number? " << endl;
-			menu();
+			menu(); // displays menu again to chose the type of calculation between second and thrid number
 			while (!(cin >> answer) || answer > 5 || answer <= 0) {
 				cout << "please enter valid number" << endl;
 				menu();
@@ -64,26 +71,26 @@ int main() {
 				cin.ignore(123, '\n');
 			}
 
-			if (answer == 5) {
+			if (answer == 5) { // second option for the user to exit program if they made a mistake
 				return 0;
 			}
 
 
-			triple(uinput1, uinput2, uinput3, uanswer, answer);
+			triple(uinput1, uinput2, uinput3, uanswer, answer); // calls on the calculation function to use the numbers inputted by user for calculation
 
-			double returnedtotal = triple(uinput1, uinput2, uinput3, uanswer, answer);
-			cout << returnedtotal << endl << endl;
+			double returnedtotal = triple(uinput1, uinput2, uinput3, uanswer, answer); // makes this value equal to the returned value (total) from the function
+			cout << returnedtotal << endl << endl; // two endl to separate this from the next calculation
 		}
 	}
 }
 
-void menu() {
-	cout << "pick an option" << endl << "1. addition" << endl << "2. subtraction" << endl << "3. multiplication" << endl << "4. division" << endl << "5. exit" << endl;
+void menu() { // function that when called (menu();) will output the cout
+	cout << "pick an option" << endl << "1. addition" << endl << "2. subtraction" << endl << "3. multiplication" << endl << "4. division" << endl << "5. exit" << endl; // prompts the user with the options for calculator
 
 }
 double calculation(double input1, double input2, double answer) {
 	double total = 0;
-	if (answer == 1) {
+	if (answer == 1) { // if the user inputted 1 at the start of the program then it will use addition etc
 		total = input1 + input2;
 	}
 	else if (answer == 2) {
@@ -95,19 +102,16 @@ double calculation(double input1, double input2, double answer) {
 	else if (answer == 4) {
 		total = input1 / input2;
 	}
-	else {
-		cout << "input valid number" << endl;
-	}
-	return total;
+	return total; // exits function and returns what the total became after the if statement
 
 }
 double triple(double input1, double input2, double input3, double answer, double answer2) {
 	double total = 0;
-	if (answer == 1) {
-		if (answer2 == 1) {
+	if (answer == 1) { // if the user inputted 1 at the start of the program then it will use addition etc for first number
+		if (answer2 == 1) { // if the user inputted 1 after wanting to use three numbers then it will use addition 
 			total = input1 + input2 + input3;
 		}
-		if (answer2 == 2) {
+		if (answer2 == 2) { // but if the user inputted 2 after wanting to use three numbers it will use subtraction between number 2 and 3 
 			total = input1 + input2 - input3;
 		}
 		if (answer2 == 3) {
@@ -118,7 +122,7 @@ double triple(double input1, double input2, double input3, double answer, double
 		}
 	}
 	else if (answer == 2) {
-		if (answer2 == 1) {
+		if (answer2 == 1) { // repeated so that all possible combinations can be performed
 			total = input1 - input2 + input3;
 		}
 		if (answer2 == 2) {
@@ -159,8 +163,5 @@ double triple(double input1, double input2, double input3, double answer, double
 			total = input1 / input2 / input3;
 		}
 	}
-	else {
-		cout << "input valid number" << endl;
-	}
-	return total;
+	return total; // exits function and returns what the total became after the if statement
 }
---
+++ test/vector test/vector test/vector test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	vector<char> alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+	for (int i = 0; i < 26; i++) {
+		cout << alphabet[i] << ", ";
+	}
+	vector<int> odd;
+}
