359a0b6dba9f9fbc987c19f54095ff7eebdc49de
Teddeakin <ted.deakin23@bathspa.ac.uk>
2024-01-10 16:40:35 +0000

    v

--- test/vector test/vector test/vector test.cpp
+++ test/vector test/vector test/vector test.cpp
@@ -1,20 +1,51 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<string>
 
 using namespace std;
 
+void menu();
+double user_choice(int choice);
+
 int main() {
+	int choice;
+	double user_money;
 
-	int a = 0;
-	vector<char> alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-	for (int i = 0; i < 26; i++) {
-		cout << alphabet[i] << ", ";
+	cout << "How much money do you have on you right now in pounds?" << endl;
+	cin >> user_money;
+
+	menu();
+	cout << "what food or drink would you like?" << endl;
+	cin >> choice;
+	user_choice(choice);
+
+	double payments = user_choice(choice);
+
+}
+
+void menu() {
+	string menu[3][5] = {
+		{"sprite", "coke", "fanta", "monster", "water"},
+		{"twix", "mars", "crunchie", "aero", "brunch oats"},
+		{"walkers", "mccoys", "sensations", "doritos", "monster munch"},
+	};
+	int count = 1;
+	cout << "MENU:" << endl << "=============" << endl;
+	for (int i = 0; i < 3; i++) {
+		for (int a = 0; a < 5; a++) {
+			cout << count << ": " << menu[i][a] << ", ";
+			count++;
+		}
+		cout << endl;
 	}
-	cout << "press any key to continue" << endl;
-	cin >> a;
-	vector<int> odd;
-	system("cls");
+	cout << endl;
+}
 
-
+double user_choice(int choice) {
+	double payment = 0.0;
+	if (choice == 1) {
+		cout << "DISPENCING SPRITE" << endl;
+		payment = 1.20;
+	}
+	return payment;
 }
